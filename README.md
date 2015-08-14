9de61acbc7ab7c1ea3b6f9507af8ec9c6583e713
Renato Marroquin <renatoj.marroquin@gmail.com>
2015-08-14 01:04:38 -0700

    Adding cmd line options

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 To run it use:
 
-mvn clean compile exec:java -Dexec.mainClass=org.apache.activemq.messaging.App
+mvn clean compile exec:java -Dexec.mainClass=org.apache.activemq.messaging.App -Dexec.args="tcp://localhost:61616 queue1"
--- src/main/java/org/apache/activemq/messaging/App.java
+++ src/main/java/org/apache/activemq/messaging/App.java
@@ -13,8 +13,10 @@ public class App
     public static void main( String[] args ) throws javax.jms.JMSException
     {
         // setup the connection to ActiveMQ
-        ConnectionFactory factory = new ActiveMQConnectionFactory(brokerURL);
-        Producer producer = new Producer(factory, "test");
+        //ConnectionFactory factory = new ActiveMQConnectionFactory(brokerURL);
+        ConnectionFactory factory = new ActiveMQConnectionFactory(args[0]);
+        //Producer producer = new Producer(factory, "test");
+        Producer producer = new Producer(factory, args[1]);
         System.out.println( "Running producer." );
         producer.run();
         System.out.println( "Closing producer." );
